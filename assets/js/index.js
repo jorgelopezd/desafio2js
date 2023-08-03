d2c7936db2ba8b278615d153fe34716771729845
Jorge Lopez <jorge_lopezd@hotmail.com>
2023-08-03 16:30:31 -0400

    se agrega desafio 1

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+let bordeimg = false;
+
+//se crea la funcion de toogle 
+function toggleBorder() {
+    let image = document.getElementById("imgdesafio1");
+    
+    if (bordeimg === false) {
+        image.classList.remove("clicked");
+    } else {
+        image.classList.add("clicked");
+    }
+
+    //evalua esta activado o desactivado el borde de la imagen haciendo clic en ella
+    bordeimg = !bordeimg;
+}
